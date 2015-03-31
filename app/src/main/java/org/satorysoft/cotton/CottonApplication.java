429fdd20477ec71f398728f0485e9ec669adc8d9
Viacheslav Okolitiy <okolitiy.viacheslav@gmail.com>
2015-04-01 01:41:03 +0300

    Initial

---
+++ app/src/main/java/org/satorysoft/cotton/CottonApplication.java
@@ -0,0 +1,13 @@
+package org.satorysoft.cotton;
+
+import android.app.Application;
+
+/**
+ * Created by viacheslavokolitiy on 01.04.2015.
+ */
+public class CottonApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ app/src/main/java/org/satorysoft/cotton/MainActivity.java
@@ -0,0 +1,15 @@
+package org.satorysoft.cotton;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by viacheslavokolitiy on 01.04.2015.
+ */
+public class MainActivity extends ActionBarActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
