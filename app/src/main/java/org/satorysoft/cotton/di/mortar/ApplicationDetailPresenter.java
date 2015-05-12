b3e18ff4ea273031876cc9fe34e821ca910b1a0c
Viacheslav Okolitiy <okolitiy.viacheslav@gmail.com>
2015-05-12 15:38:22 +0300

    Minor changes

--- app/src/main/java/org/satorysoft/cotton/di/mortar/ApplicationDetailPresenter.java
+++ app/src/main/java/org/satorysoft/cotton/di/mortar/ApplicationDetailPresenter.java
@@ -1,17 +1,22 @@
 package org.satorysoft.cotton.di.mortar;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.PermissionInfo;
+import android.database.Cursor;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.widget.ImageView;
 
 import org.satorysoft.cotton.adapter.PermissionListAdapter;
 import org.satorysoft.cotton.core.event.PopulateCardViewEvent;
+import org.satorysoft.cotton.db.contract.ScannedApplicationContract;
 import org.satorysoft.cotton.di.component.CoreComponent;
 import org.satorysoft.cotton.di.component.DaggerCoreComponent;
 import org.satorysoft.cotton.di.module.CoreModule;
@@ -83,5 +88,27 @@ public class ApplicationDetailPresenter extends ViewPresenter<ApplicationDetailV
 
     @Override public Drawable restoreDrawable(byte[] bytes){
         return new BitmapDrawable(BitmapFactory.decodeByteArray(bytes, 0, bytes.length));
+    }
+
+    public void deleteApplication(Context context, RobotoTextView applicationName) {
+        Cursor cursor = context.getContentResolver().query(ScannedApplicationContract.CONTENT_URI,
+                null, ScannedApplicationContract.APPLICATION_NAME + "=?",
+                new String[]{applicationName.getText().toString()}, null);
+        String packageName = "";
+        if(cursor != null && cursor.getCount() > 0 && cursor.moveToFirst()){
+            do {
+                packageName = cursor.getString(cursor.getColumnIndex(ScannedApplicationContract.PACKAGE_NAME));
+            } while (cursor.moveToNext());
+        }
+
+        if(cursor != null){
+            cursor.close();
+        }
+
+        if(!TextUtils.isEmpty(packageName)){
+            Intent intent = new Intent(Intent.ACTION_DELETE);
+            intent.setData(Uri.parse("package:" + packageName));
+            context.startActivity(intent);
+        }
     }
 }
