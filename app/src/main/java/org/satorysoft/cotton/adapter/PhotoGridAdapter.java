3291fd269d5c43fa40020d8d5a92dbf68d44c270
Viacheslav Okolitiy <okolitiy.viacheslav@gmail.com>
2015-05-22 15:27:34 +0300

    CAT

--- app/src/main/java/org/satorysoft/cotton/adapter/PhotoGridAdapter.java
+++ app/src/main/java/org/satorysoft/cotton/adapter/PhotoGridAdapter.java
@@ -64,49 +64,9 @@ public class PhotoGridAdapter extends BaseAdapter {
             imageView = (ImageView) convertView;
         }
 
-        /*Bitmap bm = decodeSampledBitmapFromUri(imageURLs.get(position), Constants.REQUIRED_WIDTH,
-                Constants.REQUIRED_HEIGHT);*/
-
         Uri uri = Uri.fromFile(new File(imageURLs.get(position)));
 
         Picasso.with(context).load(uri).resize(96,96).centerCrop().into(imageView);
         return imageView;
     }
-
-    /*public Bitmap decodeSampledBitmapFromUri(String path, int reqWidth, int reqHeight) {
-
-        Bitmap bm = null;
-        // First decode with inJustDecodeBounds=true to check dimensions
-        final BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inJustDecodeBounds = true;
-        BitmapFactory.decodeFile(path, options);
-
-        // Calculate inSampleSize
-        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
-
-        // Decode bitmap with inSampleSize set
-        options.inJustDecodeBounds = false;
-        bm = BitmapFactory.decodeFile(path, options);
-
-        return bm;
-    }
-
-    public int calculateInSampleSize(
-
-        BitmapFactory.Options options, int reqWidth, int reqHeight) {
-        // Raw height and width of image
-        final int height = options.outHeight;
-        final int width = options.outWidth;
-        int inSampleSize = 1;
-
-        if (height > reqHeight || width > reqWidth) {
-            if (width > height) {
-                inSampleSize = Math.round((float)height / (float)reqHeight);
-            } else {
-                inSampleSize = Math.round((float)width / (float)reqWidth);
-            }
-        }
-
-        return inSampleSize;
-    }*/
 }
