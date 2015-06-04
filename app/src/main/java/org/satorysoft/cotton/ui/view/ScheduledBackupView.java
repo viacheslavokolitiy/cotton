fbb687ea847c4ae6f06c26bdf7f4bbe569856f27
Viacheslav Okolitiy <okolitiy.viacheslav@gmail.com>
2015-06-04 17:34:20 +0300

    Add markup for scheduled backup activity

--- app/src/main/java/org/satorysoft/cotton/ui/view/ScheduledBackupView.java
+++ app/src/main/java/org/satorysoft/cotton/ui/view/ScheduledBackupView.java
@@ -2,15 +2,19 @@ package org.satorysoft.cotton.ui.view;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.widget.CheckBox;
 import android.widget.RelativeLayout;
 
+import org.satorysoft.cotton.R;
 import org.satorysoft.cotton.di.component.mortar.ScheduledBackupComponent;
 import org.satorysoft.cotton.di.mortar.ScheduledBackupPresenter;
+import org.satorysoft.cotton.ui.view.widget.RobotoTextView;
 import org.satorysoft.cotton.util.DaggerService;
 
 import javax.inject.Inject;
 
 import butterknife.ButterKnife;
+import butterknife.FindView;
 
 /**
  * Created by viacheslavokolitiy on 02.06.2015.
@@ -18,6 +22,15 @@ import butterknife.ButterKnife;
 public class ScheduledBackupView extends RelativeLayout {
     @Inject
     protected ScheduledBackupPresenter scheduledBackupPresenter;
+
+    @FindView(R.id.checkbox_select_photo)
+    protected CheckBox selectPhotoCategory;
+    @FindView(R.id.checkbox_select_call_log)
+    protected CheckBox selectCallLogCategory;
+    @FindView(R.id.txt_setup_scheduled_backup)
+    protected RobotoTextView setupScheduledBackup;
+    @FindView(R.id.txt_scheduled_backup_date)
+    protected RobotoTextView scheduledBackupDateTime;
 
     public ScheduledBackupView(Context context) {
         super(context);
@@ -44,5 +57,6 @@ public class ScheduledBackupView extends RelativeLayout {
     protected void onDetachedFromWindow() {
         super.onDetachedFromWindow();
         scheduledBackupPresenter.dropView(this);
+        ButterKnife.unbind(this);
     }
 }
